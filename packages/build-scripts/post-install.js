c6da77297d61a50044bcc1de74c56945a2eece28
Owen Rossi-Keen <owenrossikeen@gmail.com>
2024-11-19 16:45:25 -0500

    Roughly functional post-install script for aggregation

--- packages/build-scripts/post-install.js
+++ packages/build-scripts/post-install.js
@@ -1,9 +1,12 @@
 const { startAtRoot } = require("./start-at-root");
-
-const path = require("path");
 const fs = require("fs");
 
-(() =>
-  startAtRoot((rootPath, pkgJson) => {
-    console.log(`Post-Install script run from package [${pkgJson.name}]`);
-  }))();
+startAtRoot(async (rootPath, modulePath, pkgJson) => {
+  fs.writeFileSync(
+    `${modulePath}/post-install.log`,
+    `Post-Install script run from package [${pkgJson.name}]\nRoot Path: ${rootPath}\nModule Path: ${modulePath}\n`
+  );
+  console.log(`Post-Install script run from package [${pkgJson.name}]`);
+
+  // This is where we'll aggregate certain modules into one export... Probably a good idea to make this typescript later.
+});
--- packages/build-scripts/start-at-root.js
+++ packages/build-scripts/start-at-root.js
@@ -1,12 +1,15 @@
 const path = require("path");
 
-export function startAtRoot(callback) {
-  const rootPath = path.resolve(process.cwd());
-  const pkgJson = require(path.resolve(rootPath, "./package.json"));
+module.exports.startAtRoot = function startAtRoot(callback) {
+  const modulePath = path.resolve(process.cwd());
+  const rootPath = modulePath.split("node_modules")[0];
+  const pkgJson = require(path.resolve(modulePath, "./package.json"));
 
   if (!rootPath || !pkgJson) {
     throw new Error(`Unable to find rootPath or pkgJson!`);
   }
 
-  return callback(rootPath, pkgJson);
-}
+  return callback(rootPath, modulePath, pkgJson).then(() => {
+    return "run";
+  });
+};
