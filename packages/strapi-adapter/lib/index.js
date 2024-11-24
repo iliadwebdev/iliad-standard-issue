3dc5f3d2ee494b365dc4cf53bf498c8c6fac1cc6
Owen Rossi-Keen <owenrossikeen@gmail.com>
2024-11-23 20:53:14 -0500

    Added readme to Strapi Adapter

--- packages/strapi-adapter/lib/index.js
+++ packages/strapi-adapter/lib/index.js
@@ -1,26 +1,26 @@
 import {
   StrapiInstance_default
-} from "./chunk-3LQKTEGR.js";
+} from "./chunk-7CRJWFUA.js";
 import {
   legacy_default
-} from "./chunk-DQHJMNAO.js";
+} from "./chunk-LYSBQDE6.js";
 import "./chunk-7P62754R.js";
 import "./chunk-AZ7NCBCO.js";
-import "./chunk-VSAOPB5A.js";
+import "./chunk-FW7Y72NA.js";
 import "./chunk-Q76N7JTV.js";
 import "./chunk-6TCAKOC4.js";
 import "./chunk-L3CB575P.js";
-import "./chunk-AANWBPDY.js";
-import "./chunk-DJHDSS4S.js";
-import "./chunk-6LFT4IRP.js";
+import "./chunk-2QSIRKTA.js";
+import "./chunk-EWC3LOEU.js";
 import "./chunk-KX23GZAT.js";
-import "./chunk-6R3UZR5R.js";
-import "./chunk-HKNJOMU2.js";
+import "./chunk-6LFT4IRP.js";
 import "./chunk-4AHPJ27J.js";
 import {
   utils_default
 } from "./chunk-EGZ6UF72.js";
-import "./chunk-2QSIRKTA.js";
+import "./chunk-AANWBPDY.js";
+import "./chunk-6R3UZR5R.js";
+import "./chunk-HKNJOMU2.js";
 import "./chunk-PKBMQBKP.js";
 
 // src/index.ts
